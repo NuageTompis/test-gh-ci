aaaf2a8bb13b287c74b4083eb2127a38fa17449b
Simon PAUGET <simon.pauget@depinfonancy.net>
2025-12-18 15:26:59 +0100

    test cache miss

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-const isOdd = require("is-odd");
-
 const run = () => {
-  const values_to_test = [3, 6];
-  const results = values_to_test.map((v) => isOdd(v));
-  for (i = 0; i < results.length; i++) {
-    console.log(`is ${values_to_test[i]} odd ? `, results[i] ? "yes" : "no");
-  }
+  console.log("hello");
 };
 
 module.exports = {
